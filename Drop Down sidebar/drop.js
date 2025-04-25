614b32b9cc3f871a924f18c81a1fb9648a98539a
harinee-m <neelaharinee05@gmail.com>
2025-04-25 14:36:05 +0530

    Add files via upload

---
+++ Drop Down sidebar/drop.js
@@ -0,0 +1,13 @@
+const selectBtn = document.getElementById('select-btn');
+const text = document.getElementById('text');
+const option = document.getElementsByClassName('option');
+
+selectBtn.addEventListener('click',function(){
+    selectBtn.classList.toggle('active');
+});
+
+for(options of option){
+    options.onclick = function(){
+        text.innerHTML = this.textContent;
+    }
+}
